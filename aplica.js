e41e088e232b911674be9306afcf93d7d17d34ba
Carlos Aluisa <carlitosaluisa@hotmail.com>
2026-08-31 22:06:03 -0500

    Implementar renderizado dinamico de habitos

--- aplica.js
+++ aplica.js
@@ -30,3 +30,43 @@ const mostrarHabito = (habito) => {
 habitos.forEach((habito) => {
     mostrarHabito(habito);
 });
+
+const listaHabitos = document.getElementById("listaHabitos");
+
+const renderizarHabitos = () => {
+
+    listaHabitos.innerHTML = "";
+
+    habitos.forEach((habito) => {
+
+        const { id, nombre, completado } = habito;
+
+        const li = document.createElement("li");
+
+        li.className = `
+            list-group-item
+            d-flex
+            justify-content-between
+            align-items-center
+            ${completado
+                ? "list-group-item-success"
+                : "list-group-item-light"}
+        `;
+
+        li.dataset.id = id;
+
+        const texto = document.createElement("span");
+
+        texto.textContent = nombre;
+
+        if (completado) {
+            texto.classList.add("text-decoration-line-through");
+        }
+
+        li.appendChild(texto);
+
+        listaHabitos.appendChild(li);
+    });
+};
+
+renderizarHabitos();
